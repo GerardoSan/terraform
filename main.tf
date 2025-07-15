5f9db81ab96487e5f601963db99230bf85ad8c68
gerardo <grbios_17@hotmail.com>
2025-07-15 18:45:46 -0500

    cambio 2

--- main.tf
+++ main.tf
@@ -1,7 +1,16 @@
-provider "aws" {
-  region = "us-east-1" 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
 resource "aws_s3_bucket" "mi_bucket" {
   bucket = "grbios141016" 
 }
