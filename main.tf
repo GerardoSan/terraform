7573360352f331b11abac33873b88535088eb6e5
gerardo <grbios_17@hotmail.com>
2025-07-15 18:53:08 -0500

    cambio 6

--- main.tf
+++ main.tf
@@ -7,10 +7,15 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
+
 resource "aws_s3_bucket" "mi_bucket" {
-  bucket = "grbios141016" 
+  bucket = "grbios141016" # Cambia por un nombre único si ya está en uso
+}
+
+resource "aws_s3_bucket_acl" "mi_bucket_acl" {
+  bucket = aws_s3_bucket.mi_bucket.id
+  acl    = "private"
 }
