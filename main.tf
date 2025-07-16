c7660dfc5756d912680cd59b42c56dcc0382a57a
gerardo <grbios_17@hotmail.com>
2025-07-15 21:10:50 -0500

    cambio 12

--- main.tf
+++ main.tf
@@ -11,7 +11,58 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "mi_bucket" {
-  bucket = "grbios141016" 
+resource "aws_instance" "nginx_server" {
+  ami = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "nginx-server"
+  }
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt update
+    sudo apt install nginx -y
+    sudo systemctl start nginx
+    sudo systemctl enable nginx
+    EOF
+    key_name = aws_key_pair.mi_key.key_name
+    vpc_security_group_ids = [aws_security_group.mi_seguridad.id]
 }
 
+resource "aws_security_group" "mi_seguridad" {
+  name = "mi_seguridad"
+  description = "Mi seguridad"
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_key_pair" "mi_key" {
+  key_name = "mi_key"
+  public_key = file("nginx-server.pub")
+}
+
+
+output "public_ip" {
+  value = aws_instance.nginx_server.public_ip
+}
+
+output "public_dns" {
+  value = aws_instance.nginx_server.public_dns
+}
